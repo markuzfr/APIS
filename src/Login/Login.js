9b518e90e40aa2b72a8344e509dc843d59bd8590
markuzfr <markus.sedmak@spsjm.eu>
2024-05-21 20:07:18 +0200

    commit trojka

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -12,16 +12,16 @@ const Login = ({ onLogin }) => {
   const isFormValid = username && password;
 
   const handleSubmit = (event) => {
-      event.preventDefault();
+    event.preventDefault();
     if (!isFormValid) {
       setError('All fields are required');
       return;
     }
 
-    console.log('Email:', username);
-    console.log('Password:', password);
+/*     console.log('Email:', username);
+    console.log('Password:', password); */
 
-    setError(''); 
+    setError('');
     onLogin();
   };
 
@@ -31,46 +31,49 @@ const Login = ({ onLogin }) => {
         <h1>webReader mobile 1.4.2</h1>
       </div>
     
-    
-    <div className="login-container">
-
-      {error && <p className="error">{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="form-group-username">
-         <input 
-            type="text"
-            placeholder='Prihlasovacie meno'
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="form-group-password">
-          <input
-            type={showPassword ? 'text' : 'password'}
-            placeholder='Heslo'
-            id="password"
-            value={password}  
-            onChange={(event) => setPassword(event.target.value)}
-          />
-          <button 
-            type="button" 
-            className="show-password-btn"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            <img 
-              src={showPassword ? 'https://cdn-icons-png.flaticon.com/512/565/565655.png' : 'https://cdn-icons-png.flaticon.com/512/159/159604.png'} 
-              alt={showPassword ? 'Hide password' : 'Show password'}
+      <div className="login-container">
+        {error && <p className="error">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="form-group-username">
+            <img className="username-icon" src={'/imgs/person.png'} alt="Username" />
+            <input 
+              type="text"
+              placeholder='Prihlasovacie meno'
+              id="username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
             />
-          </button>
-        </div>
-        <div className='autoSign'>
-          <h5>Prihlásiť automaticky</h5>
-          <input type='checkbox'/>
-        </div>
-        <button type="submit" className='confirmButton' disabled={!isFormValid}>Login</button>
-      </form>
-    </div>
+          </div>
+          <div className="form-group-password">
+            <img className="password-icon" src={'/imgs/lock.png'} alt="Password" />
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder='Heslo'
+              id="password"
+              value={password}  
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <button 
+              type="button" 
+              className="show-password-btn"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <img 
+                src={showPassword ? '/imgs/closed_eye.png' : '/imgs/opened_eye.png'} 
+                alt={showPassword ? 'Hide password' : 'Show password'}
+              />
+            </button>
+          </div>
+          <div className='autoSign'>
+            <h4>Prihlásiť automaticky</h4>
+            <input type='checkbox'/>
+          </div>
+          <button type="submit" className='confirmButton' disabled={!isFormValid}>PRIHLÁSIŤ</button>
+        </form>
+      </div>
+      <div className='settingsBox'>
+        <img src="/imgs/cog.png" alt="Settings" />
+      </div>
     </>
   );
 };
