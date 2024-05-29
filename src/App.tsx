3d4a102b5325e085d36fb1d79e4baf63aeb561f8
markuzfr <markus.sedmak@spsjm.eu>
2024-05-29 10:55:00 +0200

    prepisany tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 //import Login from './components/Login/Login.js';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/index.js'; 
+import { router } from './routes/index.tsx'; 
 
 function App() {
   return <RouterProvider router={router}/>;
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import reportWebVitals from './reportWebVitals.tsx';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+  // Log performance metrics to the console
+  reportWebVitals(console.log);
+} else {
+  console.error('Root element not found');
+}
